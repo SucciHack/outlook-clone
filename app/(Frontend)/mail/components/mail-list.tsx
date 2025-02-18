b9f3a071be7f22400cffde58ef8e67f31b18738e
Kaweesi Samuel <158592803+SucciHack@users.noreply.github.com>
2025-02-18 18:17:39 +0300

    updated now

--- app/(Frontend)/mail/components/mail-list.tsx
+++ app/(Frontend)/mail/components/mail-list.tsx
@@ -4,7 +4,6 @@ import { ComponentProps } from "react";
 import { cn } from "@/lib/utils";
 import { Badge } from "@/components/ui/badge";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { Separator } from "@/components/ui/separator";
 import { useMail } from "../use-mail";
 import { Mail } from "../data";
 import { formatDistanceToNow } from "date-fns";
